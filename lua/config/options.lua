4e2d630bb7015c5f1d1af6cc4a956c4b2defc092
Omar Zhadykov <omar.zhadykov@montel.energy>
2026-05-26 16:45:28 +0200

    feature: added visibility for dot files

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,1 +1,20 @@
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+
 vim.cmd.colorscheme("jb")
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.cursorline = true
+vim.opt.signcolumn = "yes"
+
+vim.opt.wrap = false
+vim.opt.scrolloff = 8
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.termguicolors = true
